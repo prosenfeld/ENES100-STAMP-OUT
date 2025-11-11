be9ffc6f24f397515f4a47ed82e5ed546df818e0
prosenfeld <jpinchasrosenfeld@gmail.com>
2025-11-11 16:06:45 -0500

    turning testing

--- OTV_Code_Ver1/OTV_Code_Ver1.ino
+++ OTV_Code_Ver1/OTV_Code_Ver1.ino
@@ -12,287 +12,305 @@ const int LEFT_MOTOR_IN2  = 19;     // D19
 
 const int SERVO_PWM = 3;
 
+const int ARM_UP = 200;
+const int ARM_DOWN = 150;
+
+// Arm up - 200
+// arm down - 150
+
 
 
 void setup() {
-    
-    float distanceToObstacle = 0.3;
-    boolean startedTop = false; 
 
-    // Enes100.begin("STAMP Out!", FIRE, 3, 1116, 8, 9);
-    // Tank.begin();
+  float distanceToObstacle = 0.3;
+  boolean startedTop = false;
 
-
-    // setup motor pins
-    
-    // Right motor pins
-    pinMode(RIGHT_MOTOR_ENABLE, OUTPUT);
-    pinMode(RIGHT_MOTOR_IN1, OUTPUT);
-    pinMode(RIGHT_MOTOR_IN2, OUTPUT);
-
-    // Left motor pins
-    pinMode(LEFT_MOTOR_ENABLE, OUTPUT);
-    pinMode(LEFT_MOTOR_IN1, OUTPUT);
-    pinMode(LEFT_MOTOR_IN2, OUTPUT);
-    
-    // arm servo
-    pinMode(SERVO_PWM,OUTPUT);
-
-    // turn right - for MS5 turning test
-    turnRight();
-    
-    // servo testing:
-    // setServo(127);
-
-    
-
- 
+  Enes100.begin("STAMP Out!", FIRE, 24, 1116, 4, 2);
+  // Tank.begin();
 
 
+  // setup motor pins
 
-    
-    // // handle initial position -> mission site 
-    // if(Enes100.getY() < 1){
-        
-    //     // This will need tuning. Ultrasonic probably should be used.
-        
-    //      driveToPoint(0.55,1.55,1.571);
-    // } else {
-        
-    //     driveToPoint(0.55,0.55,-1.571);
-    // }
-    
-    // turnAngle(0);
-    // // do mission things here
-    // delay(1000);
-    
-    
-    // // drive forward until we find an obstacle
-    // while((Tank.readDistanceSensor(1) > distanceToObstacle || Tank.readDistanceSensor(1) == -1) && Enes100.getY() < 2.7 ){
-    //     driveForward();
-    //     delay(1);
-    // }
-    // stopDriving();
-    // // edge case of just the rumble
-    // if(Enes100.getX() > 3.6){
+  // Right motor pins
+  pinMode(RIGHT_MOTOR_ENABLE, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN1, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN2, OUTPUT);
+
+  // Left motor pins
+  pinMode(LEFT_MOTOR_ENABLE, OUTPUT);
+  pinMode(LEFT_MOTOR_IN1, OUTPUT);
+  pinMode(LEFT_MOTOR_IN2, OUTPUT);
+
+  // arm servo
+  pinMode(SERVO_PWM, OUTPUT);
+
+  // turn right - for MS5 turning test
+  //  driveForward();
+
+  // arm up
+   setServo(ARM_UP);
+
+//   driveForward();
+//   delay(10000);
+//   setServo(ARM_DOWN);
+    turnAngle(1.57);
+//
    
-    //     return;
-    // } else if( Enes100.getX() > 2.7){
-    //     limbo();
-    //     return;
-    // }
-    
-    // // turn  to try to find an opening
-    // if(Enes100.getY() < 1){
-    //     // turnAngle(-1.571);
-    //     driveToPoint(Enes100.getX(),Enes100.getY() + 0.5,0);
-        
-    //       // if we are open, move forward. if not, do that again.
-            
-    //         if(Tank.readDistanceSensor(1) > 0.5){
-                
-    //             driveToPoint(3,Enes100.getY(),0);
-        
-    //         } else{
-    //             // move down another row
-    //             driveToPoint(Enes100.getX(),Enes100.getY() + 0.5,0);
-                
-    //         }
-    
-    // } else{
-    //     // turnAngle(1.571)
-    //     driveToPoint(Enes100.getX(),Enes100.getY() - 0.5,0);
-    //     Enes100.println(Tank.readDistanceSensor(1));
-        
-    //        if(Tank.readDistanceSensor(1) > 0.5 || Tank.readDistanceSensor(1) == -1){
-                
-    //             driveToPoint(3,Enes100.getY(),0);
-        
-    //         } else{
-    //             // move down another row
-    //             driveToPoint(Enes100.getX(),Enes100.getY() - 0.5,0);
-    //             // go forward
-    //             driveToPoint(3,Enes100.getY(),0);
 
-                
-                
-                
-    //         }
 
-    // }
-    
-    // limbo();
-    
- 
-    
-    
+
+
+
+
+
+  // // handle initial position -> mission site
+  // if(Enes100.getY() < 1){
+
+  //     // This will need tuning. Ultrasonic probably should be used.
+
+  //      driveToPoint(0.55,1.55,1.571);
+  // } else {
+
+  //     driveToPoint(0.55,0.55,-1.571);
+  // }
+
+  // turnAngle(0);
+  // // do mission things here
+  // delay(1000);
+
+
+  // // drive forward until we find an obstacle
+  // while((Tank.readDistanceSensor(1) > distanceToObstacle || Tank.readDistanceSensor(1) == -1) && Enes100.getY() < 2.7 ){
+  //     driveForward();
+  //     delay(1);
+  // }
+  // stopDriving();
+  // // edge case of just the rumble
+  // if(Enes100.getX() > 3.6){
+
+  //     return;
+  // } else if( Enes100.getX() > 2.7){
+  //     limbo();
+  //     return;
+  // }
+
+  // // turn  to try to find an opening
+  // if(Enes100.getY() < 1){
+  //     // turnAngle(-1.571);
+  //     driveToPoint(Enes100.getX(),Enes100.getY() + 0.5,0);
+
+  //       // if we are open, move forward. if not, do that again.
+
+  //         if(Tank.readDistanceSensor(1) > 0.5){
+
+  //             driveToPoint(3,Enes100.getY(),0);
+
+  //         } else{
+  //             // move down another row
+  //             driveToPoint(Enes100.getX(),Enes100.getY() + 0.5,0);
+
+  //         }
+
+  // } else{
+  //     // turnAngle(1.571)
+  //     driveToPoint(Enes100.getX(),Enes100.getY() - 0.5,0);
+  //     Enes100.println(Tank.readDistanceSensor(1));
+
+  //        if(Tank.readDistanceSensor(1) > 0.5 || Tank.readDistanceSensor(1) == -1){
+
+  //             driveToPoint(3,Enes100.getY(),0);
+
+  //         } else{
+  //             // move down another row
+  //             driveToPoint(Enes100.getX(),Enes100.getY() - 0.5,0);
+  //             // go forward
+  //             driveToPoint(3,Enes100.getY(),0);
+
+
+
+
+  //         }
+
+  // }
+
+  // limbo();
+
+
+
+
 }
 
 // loop function
 void loop() {
-        delay(100);
-        // setServo(0);
-        // delay(1000);
-        // setServo(127);
-        // delay(1000);
+
+//  Enes100.println("X");
+//  Enes100.println(Enes100.getX());
+//  Enes100.println("Y");
+//  Enes100.println(Enes100.getY());
+//  Enes100.println("Theta:");
+//  Enes100.println(Enes100.getTheta());
+
+  delay(1000);
+
+
 }
 
 
 // Servo for the arm
-void setServo(int pwm){
-    analogWrite(SERVO_PWM,pwm);
+void setServo(int pwm) {
+  analogWrite(SERVO_PWM, pwm);
 }
 
 // do the limbo
-void limbo(){
-          // drive to line with limbo
-        driveToPoint(3,1.5,0);
-        // go through limbo
-        driveToPoint(3.7,1.5,0);
-        // Done!
-        Enes100.println("done!");
+void limbo() {
+  // drive to line with limbo
+  driveToPoint(3, 1.5, 0);
+  // go through limbo
+  driveToPoint(3.7, 1.5, 0);
+  // Done!
+  Enes100.println("done!");
 }
 
 
 // function to turn to a specific given angle
-void turnAngle(float angle){
+void turnAngle(float angle) {
 
   Enes100.print("Turning to angle");
   Enes100.print(angle);
-  
-  
+
+
   // Angle tolerance. Will need tuning.
-    float delta = 0.1;
-    
-   // if we are within error, don't move
-   if(abs(Enes100.getTheta() - angle) < delta){
-       return;
-   }
+  float delta = 0.1;
 
-    
-    
-    // Pick which way we are turning.
-    if(Enes100.getTheta() - angle < 0){
+  // if we are within error, don't move
+  if (abs(Enes100.getTheta() - angle) < delta) {
+    return;
+  }
+
+
+
+  // Pick which way we are turning.
+  if (Enes100.getTheta() - angle < 0) {
     turnLeft();
-    } else{
-        turnRight();
-    }
-    // Turn until we are within tolerance.
-    while(abs(Enes100.getTheta() - angle) > delta){
-        delay(1);
+  } else {
+    turnRight();
+  }
+  // Turn until we are within tolerance.
+  while (abs(Enes100.getTheta() - angle) > delta) {
+    delay(1);
 
-    }
-    
-    Enes100.println("done!");
-     Enes100.println(abs(Enes100.getTheta() - angle));
-    stopDriving();
-    
+  }
+
+  Enes100.println("done!");
+  Enes100.println(abs(Enes100.getTheta() - angle));
+  stopDriving();
+
 }
 
 // helper function for distance between points
-float dist(float x0, float x1, float y0, float y1){
+float dist(float x0, float x1, float y0, float y1) {
   return sqrt(pow(x0 - x1, 2) + pow(y0 - y1, 2));
 }
 
 // drive to a specific given point
-void driveToPoint(float x, float y, float theta){
-    // Calculate the angle to turn to
+void driveToPoint(float x, float y, float theta) {
+  // Calculate the angle to turn to
   float deltaX = x - Enes100.getX();
   float deltaY = y - Enes100.getY();
-  float deltaTheta = atan2(deltaY,deltaX);
+  float deltaTheta = atan2(deltaY, deltaX);
   float thetaError = 0.1;
-  
-  
+
+
   // turn to that angle
   turnAngle(deltaTheta);
-  
+
   // drive until we are close enough to that point.
   driveForward();
-  while(dist(Enes100.getX(),x,Enes100.getY(),y) > 0.1){
-      // adjust heading if needed, if we veer to a side.
-      if(abs(Enes100.getTheta() - deltaTheta) > thetaError){
-          deltaX = x - Enes100.getX();
-          deltaY = y - Enes100.getY();
-          deltaTheta = atan2(deltaY,deltaX);
-          turnAngle(deltaTheta);
-      }
-      
-      delay(1);
+  while (dist(Enes100.getX(), x, Enes100.getY(), y) > 0.1) {
+    // adjust heading if needed, if we veer to a side.
+    if (abs(Enes100.getTheta() - deltaTheta) > thetaError) {
+      deltaX = x - Enes100.getX();
+      deltaY = y - Enes100.getY();
+      deltaTheta = atan2(deltaY, deltaX);
+      turnAngle(deltaTheta);
+    }
+
+    delay(1);
   }
-  
+
   stopDriving();
-  
+
   // Get the heading to the requested one
   turnAngle(theta);
-  
-    
+
+
 }
 
 
 // Driving functions
 
-void setLeftMotorPWM(int leftMotor){
-    // Backwards
-    if (leftMotor < 0){
+void setLeftMotorPWM(int leftMotor) {
+  // Backwards
+  if (leftMotor < 0) {
 
-        digitalWrite(LEFT_MOTOR_IN1, HIGH);
-        digitalWrite(LEFT_MOTOR_IN2, LOW);
-        analogWrite(LEFT_MOTOR_ENABLE, abs(leftMotor));
-    }
-    // Forwards
-    else{
+    digitalWrite(LEFT_MOTOR_IN1, HIGH);
+    digitalWrite(LEFT_MOTOR_IN2, LOW);
+    analogWrite(LEFT_MOTOR_ENABLE, abs(leftMotor));
+  }
+  // Forwards
+  else {
 
-        digitalWrite(LEFT_MOTOR_IN1, LOW);
-        digitalWrite(LEFT_MOTOR_IN2, HIGH);
-        analogWrite(LEFT_MOTOR_ENABLE, abs(leftMotor));
-    }
-
-}
-
-void setRightMotorPWM(int rightMotor){
-    // Backwards
-    if (rightMotor < 0){
-
-        digitalWrite(RIGHT_MOTOR_IN1, HIGH);
-        digitalWrite(RIGHT_MOTOR_IN2, LOW);
-        analogWrite(RIGHT_MOTOR_ENABLE, abs(rightMotor));
-    }
-    // Forwards
-    else{
-
-        digitalWrite(RIGHT_MOTOR_IN1, LOW);
-        digitalWrite(RIGHT_MOTOR_IN2, HIGH);
-        analogWrite(RIGHT_MOTOR_ENABLE, abs(rightMotor));
-    }
+    digitalWrite(LEFT_MOTOR_IN1, LOW);
+    digitalWrite(LEFT_MOTOR_IN2, HIGH);
+    analogWrite(LEFT_MOTOR_ENABLE, abs(leftMotor));
+  }
 
 }
 
-void setDriving(int leftMotor, int rightMotor){
-        setLeftMotorPWM(leftMotor);
-        setRightMotorPWM(rightMotor);
+void setRightMotorPWM(int rightMotor) {
+  // Backwards
+  if (rightMotor < 0) {
+
+    digitalWrite(RIGHT_MOTOR_IN1, HIGH);
+    digitalWrite(RIGHT_MOTOR_IN2, LOW);
+    analogWrite(RIGHT_MOTOR_ENABLE, abs(rightMotor));
+  }
+  // Forwards
+  else {
+
+    digitalWrite(RIGHT_MOTOR_IN1, LOW);
+    digitalWrite(RIGHT_MOTOR_IN2, HIGH);
+    analogWrite(RIGHT_MOTOR_ENABLE, abs(rightMotor));
+  }
+
 }
 
-void stopDriving(){
-        setLeftMotorPWM(0);
-        setRightMotorPWM(0);
+void setDriving(int leftMotor, int rightMotor) {
+  setLeftMotorPWM(leftMotor);
+  setRightMotorPWM(rightMotor);
 }
 
-void driveForward(){
-        setLeftMotorPWM(255);
-        setRightMotorPWM(255);
+void stopDriving() {
+  setLeftMotorPWM(0);
+  setRightMotorPWM(0);
 }
 
-void driveBackward(){
-        setLeftMotorPWM(-255);
-        setRightMotorPWM(-255);
+void driveForward() {
+  setLeftMotorPWM(255);
+  setRightMotorPWM(255);
+}
+
+void driveBackward() {
+  setLeftMotorPWM(-255);
+  setRightMotorPWM(-255);
 }
 // tune turning speed based on how the vision system behaves.
-void turnLeft(){
-        setLeftMotorPWM(-255);
-        setRightMotorPWM(255);
+void turnLeft() {
+  setLeftMotorPWM(-100);
+  setRightMotorPWM(100);
 }
 
-void turnRight(){
-        setLeftMotorPWM(-255);
-        setRightMotorPWM(255);
+void turnRight() {
+  setLeftMotorPWM(-100);
+  setRightMotorPWM(100);
 }
